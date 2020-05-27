582dcb2f7013e9295d89dc5ed0f7b5698de8b9ba
Aaron Patch <ampatch@gmail.com>
2020-05-27 17:10:22 -0400

    added branch.io implementation. Deep link created for main activity (only activity)

--- app/src/main/java/com/example/branchtestproject/MainActivity.java
+++ app/src/main/java/com/example/branchtestproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.branchtestproject;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.layout_main_activity);
 
+        Uri data = this.getIntent().getData();
+        if (data != null && data.isHierarchical()) {
+            String uri = this.getIntent().getDataString();
+            Log.i("MyApp", "Deep link clicked " + uri);
+            Toast.makeText(this, "uri data = " + uri, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "data is null", Toast.LENGTH_LONG).show();
+        }
     }
 }
